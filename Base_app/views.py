855fd9198f1856e0810f7219856281c9703a0042
Gourab-Ghosh123 <ggourab2004@gmail.com>
2025-07-17 21:37:50 +0530

    Add files via upload

---
+++ Base_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def Home(request):
+    return render(request,'home.html')
+
+def AboutView():
+    pass
+
+def MenuView():
+    pass
+
+def Book_TableView():
+    pass
